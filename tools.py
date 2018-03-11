b03f6cf2db7712e004a605c5d93809eb0c6cd4b4
Sylvain Dangin <sylvain.dangin@gmail.com>
2018-03-11 21:36:52 +0100

    Ajout des tâches cron

--- tools.py
+++ tools.py
@@ -31,6 +31,28 @@ if 'darwin' in sys.platform:
 plugin_name = 'template'
 
 
+class MethodData:
+    class_file_path = ''
+    class_name = ''
+    method_name = ''
+    method_visibility = 'public'
+    method_is_static = False
+
+    def get_method_declaration(self):
+        """Obtenir la déclaration de la classe dans le fichier
+        """
+        return 'class '+self.class_name+' '
+
+    def get_method_func(self):
+        """Obtenir la déclaration de la méthode données
+        """
+        output = '    '+self.method_visibility+' '
+        if self.method_is_static:
+            output += 'static '
+        output += 'function '+self.method_name+'() {\n\n    }\n'
+        return output
+
+
 class BaseMenu:
     menu = []
 
@@ -184,10 +206,108 @@ class InfoMenu(BaseMenu):
         ))
 
 
+class FonctionnalitiesMenu(BaseMenu):
+    menu = ['Ajouter une méthode cron']
+
+    def action_1(self):
+        """Modifier le nom affiché dans les menus
+        """
+        crons = {
+            'Toutes les minutes': 'cron',
+            'Toutes les 5 minutes': 'cron5',
+            'Toutes les 15 minutes': 'cron15',
+            'Toutes les 30 minutes': 'cron30',
+            'Toutes les heures': 'cronHourly',
+            'Tous les jours': 'cronDaily'
+        }
+        keys = crons.keys()
+        choice = self.get_menu_choice(keys)
+        if choice >= 0:
+            method_data = MethodData()
+            method_data.class_file_path = os.path.join(
+                'plugin-'+plugin_name,
+                'core',
+                'class',
+                plugin_name+'.class.php')
+            method_data.class_name = plugin_name
+            method_data.method_name = crons[keys[choice]]
+            method_data.method_is_static = True
+            self.add_method(method_data)
+
+    def add_method(self, method_data):
+        """Ajoute la méthode à la classe
+        :params method_data: Données de la méthode
+        :type method_data:   MethodData
+        """
+        if self.check_class(method_data):
+            if self.check_if_method_exists(method_data):
+                self.write_method_in_class(method_data)
+            else:
+                self.print_error('La méthode existe déjà')
+        else:
+            self.print_error('Le fichier n\'existe pas')
+
+    def check_class(self, method_data):
+        """Test si la classe existe
+        :params method_data: Données de la méthode
+        :type method_data:   MethodData
+        """
+        result = False
+        if os.path.exists(method_data.class_file_path):
+            with open(method_data.class_file_path) as file_content:
+                if method_data.class_name in file_content.read():
+                    result = True
+        return result
+
+    def check_if_method_exists(self, method_data):
+        """Test si la méthode existe déjà
+        :params method_data: Données de la méthode
+        :type method_data:   MethodData
+        """
+        result = False
+        with open(method_data.class_file_path) as file_content:
+            if method_data.method_name in file_content.read():
+                result = True
+        return result
+
+    def write_method_in_class(self, method_data):
+        """Ecrit la méthode à la classe
+        :params method_data: Données de la méthode
+        :type method_data:   MethodData
+        """
+        output = []
+        bracket_count = 0
+        start_bracket_count = False
+        method_added = False
+        content = None
+        class_declaration = 'class '+method_data.class_name+' '
+        with open(method_data.class_file_path, 'r') as class_file_content:
+            content = class_file_content.readlines()
+        # Recherche de la dernière accolade de la classe
+        for line in content:
+            if not start_bracket_count and class_declaration in line:
+                start_bracket_count = True
+            if not method_added and start_bracket_count:
+                if '{' in line:
+                    bracket_count += 1
+                if '}' in line:
+                    bracket_count -= 1
+                    # Dernière accolade de la classe
+                    if bracket_count == 0:
+                        output.append(method_data.get_method_func())
+                        method_added = True
+            output.append(line)
+        # Réécrit le fichier
+        with open(method_data.class_file_path, 'w') as class_file_content:
+            for line in output:
+                class_file_content.write(line)
+
+
 class RootMenu(BaseMenu):
     menu = ['Télécharger le plugin Template',
             'Modifier l\'identifiant du plugin',
-            'Modifier les informations du plugin']
+            'Modifier les informations du plugin',
+            'Ajouter des fonctionnalités']
 
     def action_1(self):
         """Télécharge une copie du plugin Template
@@ -220,6 +340,12 @@ class RootMenu(BaseMenu):
         """
         info_menu = InfoMenu()
         info_menu.start()
+
+    def action_4(self):
+        """Lance le menu de modification des informations
+        """
+        fonctionnalities_menu = FonctionnalitiesMenu()
+        fonctionnalities_menu.start()
 
     def rename_plugin(self, current_path, old_name, new_name):
         """Remplace les occurences dans les noms des fichiers, les répertoires,
@@ -323,9 +449,29 @@ def write_current_plugin_name():
         plugin_name_file.close()
 
 
+def show_help():
+    """Affiche l'aide
+    """
+    print(sys.argv[0]+' [--plugin-name PLUGIN-NAME] [--help]')
+    print('  --help :        Affiche de menu.')
+    print('  --plugin-name : Indiquer le nom du plugin à modifier.')
+
+
+def parse_args():
+    """Analyse les arguments
+    """
+    if '--help' in sys.argv:
+        show_help()
+    if len(sys.argv) > 1:
+        global plugin_name
+        plugin_name = sys.argv[1]
+    else:
+        get_current_plugin_name()
+
+
 # Point de départ
 if __name__ == '__main__':
-    get_current_plugin_name()
+    parse_args()
 
     root_menu = RootMenu()
     root_menu.start()
