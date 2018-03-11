3f49c2ec5f370fee3d9d03bf9f12ed884145762e
Sylvain Dangin <sylvain.dangin@gmail.com>
2018-03-11 15:45:34 +0100

    Ajout de la modification des informations

--- tools.py
+++ tools.py
@@ -20,10 +20,10 @@ choice_prompt = 'Choix : '
 plugin_template_repo = \
     'https://github.com/Jeedom-Plugins-Extra/plugin-template.git'
 plugin_name_file_path = '.plugin_name'
-sed_replace = 'sed -i"" "s/{}/{}/g" {} 2> /dev/null'
+sed_replace = "sed -i'' 's/{}/{}/g' {} 2> /dev/null"
 # Version Mac OS X
 if 'darwin' in sys.platform:
-    sed_replace = 'sed -i "" "s/{}/{}/g" {} 2> /dev/null'
+    sed_replace = "sed -i '' 's/{}/{}/g' {} 2> /dev/null"
 
 ##########
 # Globales
@@ -32,6 +32,8 @@ plugin_name = 'template'
 
 
 class BaseMenu:
+    menu = []
+
     def print_error(self, msg):
         """Affiche un message d'erreur
         :params msg: Message à afficher
@@ -39,18 +41,18 @@ class BaseMenu:
         """
         print('/!\\ '+msg)
 
-    def show_menu(self, show_cancel=True):
+    def show_menu(self, menu, show_cancel=True):
         """Afficher un menu
         :params menu: Tableau des choix à afficher
         :type menu:   array
         """
         print('')
-        for index, menu_item in enumerate(self.menu):
+        for index, menu_item in enumerate(menu):
             print(str(index + 1)+'. '+menu_item)
         if show_cancel:
             print('0. '+cancel_menu)
 
-    def get_menu_choice(self):
+    def get_menu_choice(self, menu):
         """Demande à l'utilisateur de faire un choix dans un menu
         :params menu: Tableau des choix à afficher
         :type menu:   array
@@ -59,10 +61,10 @@ class BaseMenu:
         """
         loop = True
         user_choice = 999
-        menu_choice_length = len(self.menu)
+        menu_choice_length = len(menu)
 
         while (loop):
-            self.show_menu()
+            self.show_menu(menu)
             try:
                 user_choice = int(self.get_user_input(choice_prompt))
             except NameError:
@@ -94,22 +96,11 @@ class BaseMenu:
         """
         loop = True
         while loop:
-            user_choice = self.get_menu_choice()
+            user_choice = self.get_menu_choice(self.menu)
             if user_choice == -1:
                 loop = False
             else:
                 self.launch(user_choice + 1)
-
-    def launch(self):
-        """Lance l'affichage du menu.
-        Méthode abstraite
-        """
-        raise NotImplementedError
-
-
-class RootMenu(BaseMenu):
-    menu = ['Télécharger le plugin Template',
-            'Modifier le nom du plugin']
 
     """Classe contenant les actions du menu racine
     """
@@ -129,6 +120,74 @@ class RootMenu(BaseMenu):
             self.print_error(bad_command)
             return_value = False
         return return_value
+
+
+class InfoMenu(BaseMenu):
+    menu = ['Modifier le nom affiché dans les menus',
+            'Modifier la description',
+            'Modifier la licence',
+            'Modifier l\'auteur',
+            'Modifier la catégorie']
+
+    def action_1(self):
+        """Modifier le nom affiché dans les menus
+        """
+        name = self.get_user_input('Nouveau nom : ')
+        self.replace_info('name', name)
+
+    def action_2(self):
+        """Modifier la description
+        """
+        description = self.get_user_input('Nouvelle description : ')
+        self.replace_info('description', description)
+
+    def action_3(self):
+        """Modifier la licence
+        """
+        licence = self.get_user_input('Nouvelle licence : ')
+        self.replace_info('licence', licence)
+
+    def action_4(self):
+        """Modifier l'auteur
+        """
+        author = self.get_user_input('Nouvel auteur : ')
+        self.replace_info('author', author)
+
+    def action_5(self):
+        """Modifier la catégorie
+        """
+        categories = [
+            'security',
+            'automation protocol',
+            'programming',
+            'organization',
+            'weather',
+            'communication',
+            'devicecommunication',
+            'multimedia',
+            'wellness',
+            'monitoring',
+            'health',
+            'nature',
+            'automatisation',
+            'energy'
+        ]
+        category = self.get_menu_choice(categories)
+        if category >= 0:
+            self.replace_info('category', categories[category])
+
+    def replace_info(self, key, new_value):
+        os.system(sed_replace.format(
+            '\("'+key+'" : "\).*\(",\)',
+            '\\1'+new_value+'\\2',
+            'plugin-'+plugin_name+os.sep+'plugin_info'+os.sep+'info.json'
+        ))
+
+
+class RootMenu(BaseMenu):
+    menu = ['Télécharger le plugin Template',
+            'Modifier l\'identifiant du plugin',
+            'Modifier les informations du plugin']
 
     def action_1(self):
         """Télécharge une copie du plugin Template
@@ -155,6 +214,12 @@ class RootMenu(BaseMenu):
             plugin_name = new_name
         else:
             self.print_error('Le plugin '+plugin_name+' n\'a pas été trouvé')
+
+    def action_3(self):
+        """Lance le menu de modification des informations
+        """
+        info_menu = InfoMenu()
+        info_menu.start()
 
     def rename_plugin(self, current_path, old_name, new_name):
         """Remplace les occurences dans les noms des fichiers, les répertoires,
